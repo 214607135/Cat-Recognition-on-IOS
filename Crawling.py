75d0191fc2780e68faf6c0975c29b25647b0076e
Mythice <kuzerdu@gmail.com>
2019-04-21 18:34:19 +1000

    simple cralwer

--- Crawling.py
+++ Crawling.py
@@ -1,7 +1,14 @@
 from icrawler.builtin import GoogleImageCrawler
-
+from datetime import date
+from icrawler.builtin import FlickrImageCrawler
 google_storage = {'root_dir': 'D:/2019 Summer Term/Project/dog'}
 google_crawler = GoogleImageCrawler(parser_threads=4,
                                    downloader_threads=4,
                                    storage=google_storage)
 google_crawler.crawl(keyword='dog', max_num=100)
+
+
+flickr_crawler = FlickrImageCrawler('your_apikey', # haven't gotten yet
+                                    storage={'root_dir': 'D:/2019 Summer Term/Project/cat-flickr'})
+flickr_crawler.crawl(max_num=1000, tags='cat',
+                     group_id='68012010@N00', min_upload_date=date(2015, 5, 1))
